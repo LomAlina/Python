5ed5b2f8b4e5932d3e66dc4290be06dc811efc9b
alinauzerinova@mail.ru <alinauzerinova@mail.ru>
2022-10-05 23:07:02 +0300

    Добавили решение Задачи 4

--- Seminar_2/HW_Task4.py
+++ Seminar_2/HW_Task4.py
@@ -1,23 +1,39 @@
 # 4. Реализуйте алгоритм перемешивания списка, без использования встроеных методов (особенно SHUFFLE, без него). 
 # Можно (нужно) использовать библиотеку Random.
 
-# Алгоритм:
+# Вариант 1:
 import random
-def mix_list(list_original):
-    list = list_original[:]
-    list_length = len(list)
-    for i in range(list_length):
-        index_aleatory = random.randint(0, list_length - 1)
-        temp = list[i]
-        list[i] = list[index_aleatory]
-        list[index_aleatory] = temp
-    return list
-list = [2, 8, 4, 3, 1, 5]
-mixed_list = mix_list(list)
-print("Исходный список: ")
-print(list)
-print("Перемешанный список: ")
-print(mixed_list)
+list = []
+
+for _ in range(10):
+    list.append(random.randint(0,50))
+print('Оригинальный список: ' + str(list))
+
+for i in range(len(list)-1):
+    j = random.randint(0, len(list)-1)
+    list[i], list[j] = list[j], list[i]
+
+print('Перемешенный список: ' + str(list))
+
+
+# Вариант 2:
+# import random
+# def mix_list(list_original):
+#     list = list_original[:]
+#     list_length = len(list)
+#     for i in range(list_length):
+#         index_aleatory = random.randint(0, list_length - 1)
+#         temp = list[i]
+#         list[i] = list[index_aleatory]
+#         list[index_aleatory] = temp
+#     return list
+# list = [2, 8, 4, 3, 1, 5]
+# mixed_list = mix_list(list)
+# print("Исходный список: ")
+# print(list)
+# print("Перемешанный список: ")
+# print(mixed_list)
+
 
 # Вариант встроенной функции:
 # import random
